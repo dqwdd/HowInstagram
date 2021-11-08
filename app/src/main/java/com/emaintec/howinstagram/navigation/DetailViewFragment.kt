9c3cffeb4d9f59aff6064653a194f163ae826dac
dqwdd <dqwdd@naver.com>
2021-11-08 23:17:19 +0900

    DetailViewFragment.kt 생성

---
+++ app/src/main/java/com/emaintec/howinstagram/navigation/DetailViewFragment.kt
@@ -0,0 +1,23 @@
+package com.emaintec.howinstagram.navigation
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.emaintec.howinstagram.R
+
+class DetailViewFragment : Fragment() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_detail_view, container, false)
+    }
+
+}
