2d33a52a926b8022aea7b65528c4f39364e0a439
dqwdd <dqwdd@naver.com>
2021-11-08 23:21:42 +0900

    AlarmFragment.kt GridFragment.kt UserFragment.kt 생성 후 레이아웃에 각자 background 입힘

---
+++ app/src/main/java/com/emaintec/howinstagram/navigation/UserFragment.kt
@@ -0,0 +1,23 @@
+package com.emaintec.howinstagram.navigation
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.emaintec.howinstagram.R
+
+class UserFragment : Fragment() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_user, container, false)
+    }
+
+}
